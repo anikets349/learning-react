c6692cd4f82fdeb432db5b0f1a0556294a9b6e35
Aniket Saha <anikets349@gmail.com>
2022-03-16 12:07:06 +0530

    Removed unnecessary files

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// stateless functional component
+// always return JSX
+// JSX rules
+// return a single element
+// div/section/article/Fragment
+// fragment -- <React.Fragment> </React.Fragment> or simply <> </>
+// className instead of class
+// close every element
+// format
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Nested components
+
+function Greeting() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
+}
+
+const Person = () => {
+  return <h2>John Doe</h2>;
+};
+
+const Message = () => {
+  return <p>This is my message</p>;
+};
+
+// function Greeting() {
+//   return (
+//     <React.Fragment>
+//       <div className="main-div">
+//         <h1>Hello World</h1>
+//       </div>
+//       <h2>New header</h2>
+//     </React.Fragment>
+//   );
+// }
+
+// const Greeting = () => {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h1", {}, "Hello World")
+//   );
+// };
+
+ReactDom.render(<Greeting />, document.getElementById("root"));
